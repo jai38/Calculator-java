0d8ba509ce541b284ac6f22792e8d6cdb7803cf4
Jai Malani <2019jai.malani@ves.ac.in>
2021-03-15 21:35:48 +0530

    initial code

---
+++ src/com/calculator/tp.java
@@ -0,0 +1,10 @@
+package com.calculator;
+
+import javax.swing.*;
+
+public class tp {
+    private JButton button1;
+    private JPanel panel1;
+    private JRadioButton radioButton1;
+    private JTextField lllllllTextField;
+}
